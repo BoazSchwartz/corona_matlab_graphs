af2bca6b3032c48659243787efea6ca4e9d25738
BoazSchwartz <boazsh0@gmail.com>
2020-04-05 09:13:56 +0300

    automatic x and t limits, better organization of the code

--- scrpt_analyze_corona.m
+++ scrpt_analyze_corona.m
@@ -2,13 +2,13 @@ function scrpt_analyze_corona
 
 close all;
 
-str_data_to_plot = 'total_deaths'; %   'total_deaths'   'total_cases'
+what_to_plot = 'total_cases'; %   'total_deaths'   'total_cases'
 
 % get the list of countries
 list_cntrs_1st_wave = {'China', 'South Korea', 'Japan', 'Iran'};
-list_cntrs_2nd_wave = {'Italy', 'Spain', 'France', 'Germany'};
+list_cntrs_2nd_wave = {'Italy', 'Spain', 'France'};
 list_cntrs_3rd_wave = {'United Kingdom', 'Israel', 'United States', 'Australia'};
-list_cntrs_forgotten = {'Austria', 'Belgium', 'Netherlands', 'Denmark'};
+list_cntrs_forgotten = {'Germany', 'Austria', 'Belgium', 'Netherlands', 'Denmark'};
 list_cntrs_east_europe = {'Poland', 'Belarus', 'Czech Republic', 'Romania'};
 list_list_countries_to_plot = {...
     list_cntrs_1st_wave, ...
@@ -28,7 +28,8 @@ for idx = 1: length(list_list_countries_to_plot)
     list_countries_to_plot = list_list_countries_to_plot{idx};
     % plot a graph for a specific list of countries
     plot_graph_list_of_countries(...
-        crn_txt, crn_data, pop_txt, pop_data, list_countries_to_plot, str_data_to_plot);
+        crn_txt, crn_data, pop_txt, pop_data, ...
+        list_countries_to_plot, what_to_plot);
 end
 
 end
@@ -52,10 +53,11 @@ end
 
 
 function plot_graph_list_of_countries(...
-    crn_txt, crn_data, pop_txt, pop_data, list_countries_to_plot, str_data_to_plot)
+    crn_txt, crn_data, pop_txt, pop_data, ...
+    list_countries_to_plot, what_to_plot)
 
 % determine which column to used, i.e., which data to plot
-idx_data_to_plot = str_to_idx_data_to_plot(str_data_to_plot);
+idx_data_to_plot = str_to_idx_data_to_plot(what_to_plot);
 
 % allocate variables
 n_countries = length(list_countries_to_plot);
@@ -66,7 +68,8 @@ pop_all = zeros(1, n_countries);
 for idx = 1: length(list_countries_to_plot)
     % get the data for this country
     str_country = list_countries_to_plot{idx};
-    [data_idx, dates] = filter_data_by_country(str_country, crn_txt, crn_data);
+    [data_idx, dates] = filter_data_by_country(...
+        str_country, crn_txt, crn_data);
     % collect the data to arrays
     data_all{idx} = data_idx(:, idx_data_to_plot);
     dates_all{idx} = dates;
@@ -76,7 +79,8 @@ for idx = 1: length(list_countries_to_plot)
 end
 
 % plot the data
-plot_all(dates_all, data_all, pop_all, list_countries_to_plot, str_data_to_plot);
+plot_all(dates_all, data_all, pop_all, ...
+    list_countries_to_plot, what_to_plot);
 
 end
 
@@ -90,27 +94,28 @@ data_country = data(idxs, :);
 end
 
 
-function plot_all(dates_all, data_all, pop_all, list_countries_to_plot, str_data_to_plot)
+function plot_all(dates_all, data_all, pop_all, ...
+    list_countries_to_plot, what_to_plot)
 
 n_vectors = length(dates_all);
-fig = figure('name', strrep(str_data_to_plot, '_', ' '), ...
+fig = figure('name', what_to_plot, ...
     'Position', [50,50,900,900]);
-idxs_min_val = 1e6 * ones(1, n_vectors);
+first_date_to_plot = 1e9 * ones(1, n_vectors);
 hold on;
 for idx = 1: n_vectors
     % get the corona data to plot
-    dates = datenum(dates_all{idx});    
+    dates = datenum(dates_all{idx});
     data_norm = data_all{idx} / (1e3 * pop_all(idx));
     data_norm = 10 * log10(data_norm);
-    min_ratio_to_display = get_min_ratio_to_display(str_data_to_plot);
+    min_ratio_to_display = get_min_ratio_to_display(what_to_plot);
     idx_tmp = find(data_norm > min_ratio_to_display, 1);
-    if ~isempty(idx_tmp), idxs_min_val(idx) = idx_tmp; end
+    if ~isempty(idx_tmp), first_date_to_plot(idx) = dates(idx_tmp); end
     plot(dates, data_norm, 'linewidth', 2)
 end
 
-date_first = dates(min(idxs_min_val));
+date_first = min(first_date_to_plot);
 date_last = dates(end);
-set_plot_parameters(list_countries_to_plot, date_first, date_last, str_data_to_plot);
+set_plot_parameters(list_countries_to_plot, date_first, date_last, what_to_plot);
 
 filename_fig = cell2mat(list_countries_to_plot);
 filename_fig = strrep(filename_fig, ' ', '');
@@ -121,18 +126,23 @@ end
 
 
 function set_plot_parameters(...
-    list_countries_to_plot, date_first, date_last, str_data_to_plot)
+    list_countries_to_plot, date_first, date_last, what_to_plot)
 
-datetick('x','mmm-dd');
-vector_magnitude_db = get_vector_magnitude_db(str_data_to_plot);
+% parameters
+vector_magnitude_db = get_vector_magnitude_db(what_to_plot);
 vector_magnitude = 10 .^ (-vector_magnitude_db ./ 10);
+
 yticks(vector_magnitude_db);
 yticklabels(vector_magnitude);
 grid minor;
 legend(list_countries_to_plot);
-ylabel({'sick ratio'; '[one sick per...]'});
-xlim([date_first, date_last - 1]);
+
+ylabel(get_str_ylabel(what_to_plot));
+datetick();
+
 ylim([vector_magnitude_db(1), vector_magnitude_db(end)]);
+xlim([date_first, date_last - 1]);
+
 set(gca, 'fontsize', 14);
 set(gca, 'fontname', 'times');
 
@@ -145,28 +155,43 @@ list_dates = unique(textdata(2: end, 1));
 end
 
 
-function min_ratio_to_display = get_min_ratio_to_display(str_data_to_plot)
-if strcmp(str_data_to_plot, 'total_cases'), min_ratio_to_display = -60;
-elseif strcmp(str_data_to_plot, 'total_deaths'), min_ratio_to_display = -80; 
-else, disp(['data to plot not found in table! str_data_to_plot = ',...
-    str_data_to_plot]);
+function idx_data_to_plot = str_to_idx_data_to_plot(what_to_plot)
+if strcmp(what_to_plot, 'total_cases')
+    idx_data_to_plot = 3;
+elseif strcmp(what_to_plot, 'total_deaths')
+    idx_data_to_plot = 4;
+else
+    disp(['data to plot not found in table! what_to_plot = ',...
+        what_to_plot]);
 end
 end
 
 
-function idx_data_to_plot = str_to_idx_data_to_plot(str_data_to_plot)
-if strcmp(str_data_to_plot, 'total_cases'), idx_data_to_plot = 3;
-elseif strcmp(str_data_to_plot, 'total_deaths'), idx_data_to_plot = 4;
-else, disp(['data to plot not found in table! str_data_to_plot = ',...
-    str_data_to_plot]);
+function vec_mag_db = get_vector_magnitude_db(what_to_plot)
+if strcmp(what_to_plot, 'total_cases')
+    vec_mag_db = -60:5:-20;
+elseif strcmp(what_to_plot, 'total_deaths')
+    vec_mag_db = -65:5:-30;
+else
+    disp(['data to plot not found in table! what_to_plot = ',...
+        what_to_plot]);
 end
 end
 
-function vector_magnitude_db = get_vector_magnitude_db(str_data_to_plot)
-if strcmp(str_data_to_plot, 'total_cases'), vector_magnitude_db = -60: 10: -20;
-elseif strcmp(str_data_to_plot, 'total_deaths'), vector_magnitude_db = -80:10:-30;
-else, disp(['data to plot not found in table! str_data_to_plot = ',...
-    str_data_to_plot]);
+
+function min_ratio_to_display = get_min_ratio_to_display(what_to_plot)
+vector_magnitude_db = get_vector_magnitude_db(what_to_plot);
+min_ratio_to_display = vector_magnitude_db(1);
 end
 
+
+function str_ylabel = get_str_ylabel(what_to_plot)
+if strcmp(what_to_plot, 'total_cases')
+    str_ylabel = 'one confirmed case out of...';
+elseif strcmp(what_to_plot, 'total_deaths')
+    str_ylabel = 'one death out of...';
+else
+    disp(['data to plot not found in table! what_to_plot = ',...
+        what_to_plot]);
+end
 end
